5f1c3cfdf48ed9ba6b432b20f31befef70733e57
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2024-03-14 04:24:01 -0300

    Add: Pizza models and cache service

--- obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b68869a558981d6df570cce76089d6d5c5b8ca8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
