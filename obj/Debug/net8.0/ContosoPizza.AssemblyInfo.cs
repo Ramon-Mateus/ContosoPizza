2c335b580e41adde406b87cc8388af342eb7dbeb
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2024-03-19 02:02:51 -0300

    feat: Endpoint Delete and test routes

--- obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f1c3cfdf48ed9ba6b432b20f31befef70733e57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e778868afcd6f18b025b87d7604f84daf170e3f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
